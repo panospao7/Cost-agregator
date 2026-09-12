27dd5ec1f60bc814f5ab88dfd3b9554131225796
pcharalampidis <pcharalampidis@york.citycollege.eu>
2026-09-12 20:44:47 +0300

    test(db): GR-14u52 follow-up - commit the count pin 107->110 left out of the u52 commit

--- scripts/db_guard/mediation_analysis/test_fixtures.py
+++ scripts/db_guard/mediation_analysis/test_fixtures.py
@@ -11,7 +11,7 @@ GROUP_MANIFESTS = (
     "protocol_manifest.yml",
 )
 CONSOLIDATED_MANIFEST = "expected_manifest.yml"
-EXPECTED_CONSOLIDATED_ROWS = 107
+EXPECTED_CONSOLIDATED_ROWS = 110
 FIELDS = ("id", "file", "expectedResolution", "expectedProof",
           "expectedRoot", "expectation")
 
